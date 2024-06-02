28cb0237e337d205604056628aa7e05e15ad79d0
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-06-02 12:18:53 +0200

    component creation and static and dinamic styles library added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageComponent } from './component/image/image.component';
+import { ImagesComponent } from './component/images/images.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageComponent,
+    ImagesComponent
   ],
   imports: [
     BrowserModule,
